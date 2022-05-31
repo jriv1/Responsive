ad402ae07d5e417f73ec72a75759749780759325
Jorge Villanueva <jriv11011101@outlook.com>
2022-05-31 09:27:12 +0800

    Responsive Site

---
+++ app.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".togglebtn");
+const links =  document.querySelector(".l");
+
+
+
+
+function wtf(){
+    links.classList.toggle("active"); 
+    console.log("HI");
+}
